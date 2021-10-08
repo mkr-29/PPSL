bacfd2903418779afeb1b8075217401fee43054b
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-08 16:48:28 +0530

    Create 19B17CI171_P_1.c
    Problem Code

---
+++ 19B17CI171_P_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("Hello World! I am in JUIT!");   //print the desired output
+    return 0;
+}
