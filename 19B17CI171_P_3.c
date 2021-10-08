21a85f56475336dd503dedaebb4b4ea936d827b2
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-08 15:48:50 +0530

    Create 19B17CI171_P_3.c
    Source code

---
+++ 19B17CI171_P_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;   //choose a variable for type char
+    scanf("%c",&a);   //take input for the chosen variable
+    printf("%c",a);   //print the desired output
+    return 0;
+}
