37e5bee3b0281a1a0ea5f1a84a4685bd1896ea30
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-08 16:00:06 +0530

    Create 19B17CI171_P_5.c
    Code

---
+++ 19B17CI171_P_5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N1,N2;  //Set the given variables for type int
+    scanf("%d",&N1);  //Take input for the N1 variable
+    scanf("%d",&N2);  //Take input for the N2 variable
+    int R;  //Set the sum variable to type int
+    R=N1+N2;    //calculate sum
+    printf("sum of numbers is %d",R);   //print the desired output
+    return 0;
+}
