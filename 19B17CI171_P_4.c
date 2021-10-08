dd5fb1bd85e529a4b40c4136991320daa4822d68
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-08 15:45:08 +0530

    Create 19B17CI171_P_4.c
    Source code

---
+++ 19B17CI171_P_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float f;  //choose a variable for type float
+    scanf("%f",&a);   //take input from the user for the chosen variable
+    printf("Number is %0.2f",a);    //print the desired output (%0.2f is is used to print the float upto 2 places of decimal)
+    return 0;
+}
