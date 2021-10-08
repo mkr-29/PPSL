f9b17169507e2fbe15f4bbcf1a58630f70f3ac37
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-08 15:40:05 +0530

    Create 19B17CI171_P_2.c
    Source code

---
+++ 19B17CI171_P_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    unsigned int w;  //choose a variable for the input of type unsigned int
+    scanf("%d",&w); //take input from the user for the chosen variable
+    printf("Number is %d",a); //print the required output
+    return 0;
+}
