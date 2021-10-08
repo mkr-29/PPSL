33dd7014d6424b2cb64e99301cff285b9ba80b06
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-08 19:41:34 +0530

    Create 19B17CI171_P_8.c
    Problem Code

---
+++ 19B17CI171_P_8.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+    char c;   //choose a variable for type char
+    scanf("%c",&c);   //print the char
+    printf("ASCII value of %c = %d",c, c);    //print the ASCII value of the char
+}
