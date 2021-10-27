c8e732ae736ceaddedd7a79f1957ffd397f7b109
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-27 19:57:46 +0530

    Create C_LAB_EXERCISE_24.c

---
+++ C_LAB_EXERCISE_24.c
@@ -0,0 +1,15 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b,s=0;
+    scanf("%d",&a);
+    for(b=1; b<=a; b++)
+    {//change the code as you may like but dont paste it as it is in your assignemnts
+        s=s+b;
+    }
+    printf("%d",s);
+    
+}
