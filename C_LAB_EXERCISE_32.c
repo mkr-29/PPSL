35d0bdbc69bc7afb2f32121790bfa66d89ca492a
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-27 19:52:10 +0530

    Create C_LAB_EXERCISE_32.c

---
+++ C_LAB_EXERCISE_32.c
@@ -0,0 +1,16 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a,b;
+    float s=0;
+    scanf("%d",&a);
+    for(b=1; b<=a; b++)   //change the code as you may like but dont paste it as it is in your assignments
+    {
+        s=s+(float)1/(b*b);
+    }
+    printf("%0.8f",s);
+    
+}
