92210e833cdccc4a3c48898ea8b96830aafd2cb6
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-10-27 19:40:03 +0530

    Create C_LAB_EXERCISE_35.c

---
+++ C_LAB_EXERCISE_35.c
@@ -0,0 +1,14 @@
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+//please change the code as you may like. But please dont copy and paste the whole code.
+int main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for(i=1; i<=a; i++)
+    {
+        printf("*");
+        printf("\n");
+    }
+}
