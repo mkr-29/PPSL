a935acd59753894a31da99625d927336d839b3d9
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-11-17 13:48:28 +0530

    Update 19B17CI171_P_33.c

--- 19B17CI171_P_33.c
+++ 19B17CI171_P_33.c
@@ -1,19 +1,16 @@
 #include<stdio.h>
 
 int main(){
-    int i,j,a,s;
+    int i,j,a;
     scanf("%d",&a);
     if(a>=0 && a<=10){
         for(i=1; i<=a; i++){
-            for(s=1; s<=a-i; s++){
-                printf(" ");
-            }//dont copy and paste
             for(j=1; j<=i; j++){
-                printf("%d ", i);
+                printf("* ");
             }printf("\n");
         }
-    }//change the code as you may like.
+    }//chnage the code as u may like
     else{
         printf("Outside the range");
-    }
+    }//dont copy and paste to complete assignments.
 }
