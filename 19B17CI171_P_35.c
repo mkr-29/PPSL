7507cc151c2621fa45fc79ea67dcb08cca32fa8e
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-11-17 11:20:42 +0530

    Create 19B17CI171_P_35.c

---
+++ 19B17CI171_P_35.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,a,x=1;
+    scanf("%d",&a);
+    if(a>=0 && a<=10){
+        for(i=1; i<=a; i++){
+            for(j=1; j<=i; j++){
+                printf("%d ",x);
+                x+=1;
+            }printf("\n");
+        }
+    }//change the code as you may like
+    else{
+        printf("Outside the range");
+    }//dont just copy and paste
+}
