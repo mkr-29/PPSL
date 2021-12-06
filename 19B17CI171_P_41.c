6e8747dd1d13b45742e991ca6aad4dad40c2c7a3
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-12-06 13:43:22 +0530

    Update 19B17CI171_P_41.c

--- 19B17CI171_P_41.c
+++ 19B17CI171_P_41.c
@@ -1,13 +1,13 @@
 #include<stdio.h>  
   
 int main()  
-{  
+{//edit the code as you may like
     int y, z;  
     float i, x;  
    
     scanf("%d", &y);
     if(y>=0&&y<=11)
-    {
+    {//but dont just copy and paste for your assignments
         for(z = 1; z <= y; z++)  
       {  
         for(x = 5.5; x <= 7.5; x += 0.5)  
