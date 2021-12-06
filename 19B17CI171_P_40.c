ebd6eee18dde5bd58a0f1e5358b446cd4c1f9c5c
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-12-06 13:39:41 +0530

    Create 19B17CI171_P_40.c

---
+++ 19B17CI171_P_40.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+int fact(int x){
+    int f=1,i;
+    for(i=1; i<=x; i++){
+            f=f*i;
+    }
+    return f;
+}
+void prime(int x){
+    int i,p=0;
+    for(i=2; i<=x/2; i++){
+        if(x%i==0){
+            p=1;
+        }
+        else{
+            p=0;
+        }
+    }
+    if(p==1){
+        printf("Not prime\n");
+    }
+    else{
+        printf("Prime\n");
+    }
+}
+void odd_even(int z){
+    if(z%2==0){
+        printf("Even\n");
+    }
+    else{
+        printf("Odd\n");
+    }
+}
+int main()
+{
+    int mc,a;
+    scanf("%d",&mc);
+    if(mc==1){
+        scanf("%d",&a);
+        if(a<1 || a>50){
+            printf("Outside the range");
+        }
+        else{
+            printf("Factorial value is %d\n",fact(a));
+        }
+        return main();
+    }
+    else if(mc==2){
+        scanf("%d",&a);
+        if(a<1 || a>50){
+            printf("Outside the range");
+        }
+        else{
+            prime(a);
+        }
+        return main();
+    }
+    else if(mc==3){
+        scanf("%d",&a);
+        if(a<1 || a>50){
+            printf("Outside the range");
+        }
+        else{
+            odd_even(a);
+        }
+        return main();
+    }
+    else if(mc==4){
+        exit(0);
+    }
+}
