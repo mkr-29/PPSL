47f723947084ffab7b9b1706b296d098d68f11fd
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-12-08 14:02:23 +0530

    Update 19B17CI171_P_43.c

--- 19B17CI171_P_43.c
+++ 19B17CI171_P_43.c
@@ -1,15 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
 int main()
 {
-    int mc,ppy;
-    int y=0,i;
+    int mc;
+    int ppy;
+    int y=0;
+    float inv;
+    float alt;
     scanf("%d",&mc);
     scanf("%d",&ppy);
-    if(mc<100 || mc>1000 || ppy<100 || ppy>1000){
-        printf("Outside the range");
-    }//edit the cost as you may like.
-    else{
-        y=mc/120;
-        printf("Minimum life of machine %d years",y);
+    alt = mc;
+    if ( ( mc > 100 && mc < 1000 ) && ( ppy > 100 && ppy <1000 ) )
+    {
+        while(1)
+        {
+            y++;
+            alt= (float) alt * 1.12;
+            inv =(float)( (ppy*y)- (mc-100) );
+            if(alt<inv)
+            {
+                y = y-1;
+                break;
+            }
+        }
+    printf("Minimum life of machine %d years",y);
     }
-}//dont just copy and paste the code for your assignmnets
+    else
+    {
+        printf("Outside the range");
+    }
+    return 0;
+}
