229dcd2ba91fbe7bc95fa5c87a9ac84fcec61baa
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-12-08 14:10:07 +0530

    Update 19B17CI171_P_43.c

--- 19B17CI171_P_43.c
+++ 19B17CI171_P_43.c
@@ -14,18 +14,18 @@ int main()
     scanf("%d",&ppy);
     alt = mc;
     if ( ( mc > 100 && mc < 1000 ) && ( ppy > 100 && ppy <1000 ) )
-    {
+    {//dont just copy and paste
         while(1)
         {
             y++;
             alt= (float) alt * 1.12;
             inv =(float)( (ppy*y)- (mc-100) );
             if(alt<inv)
-            {
+            {//edit the code as you may like
                 y = y-1;
                 break;
             }
-        }
+        }//Source : //source: https://bit.ly/3Dnjzcq
     printf("Minimum life of machine %d years",y);
     }
     else
