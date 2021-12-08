dab70db1beade231533691fdc598b91fbab7c953
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-12-08 14:01:30 +0530

    Update 19B17CI171_P_40.c

--- 19B17CI171_P_40.c
+++ 19B17CI171_P_40.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-int fact(int x){
+void fact(int x){
     int f=1,i;
     for(i=1; i<=x; i++){
             f=f*i;
     }
-    return f;
+    printf("Factorial value is %d\n",f);
 }
 void prime(int x){
     int i,p=0;
     for(i=2; i<=x/2; i++){
         if(x%i==0){
             p=1;
+            break;
         }
         else{
             p=0;
@@ -21,7 +22,7 @@ void prime(int x){
         printf("Not prime\n");
     }
     else{
-        printf("Prime\n");
+        printf("Prime number\n");
     }
 }
 void odd_even(int z){
@@ -31,42 +32,43 @@ void odd_even(int z){
     else{
         printf("Odd\n");
     }
-}//edit the code as you may like
+}
 int main()
-{//but dont paste it as it is for the assignments.
+{
     int mc,a;
-    scanf("%d",&mc);
-    if(mc==1){
-        scanf("%d",&a);
-        if(a<1 || a>50){
-            printf("Outside the range");
+    while(mc!=4){
+        scanf("%d",&mc);
+        switch(mc){
+            case 1:
+                scanf("%d",&a);
+                if(a<1 || a>50){
+                    printf("Outside the range\n");
+                }
+                else{
+                    fact(a);
+                }
+                break;
+            case 2:
+                scanf("%d",&a);
+                if(a<1 || a>50){
+                    printf("Outside the range\n");
+                }
+                else{
+                    prime(a);
+                }
+                break;
+            case 3:
+                scanf("%d",&a);
+                if(a<1 || a>50){
+                    printf("Outside the range\n");
+                }
+                else{
+                    odd_even(a);
+                }
+                break;
+            case 4:
+                exit (0);
+                break;
         }
-        else{
-            printf("Factorial value is %d\n",fact(a));
-        }
-        return main();
-    }
-    else if(mc==2){
-        scanf("%d",&a);
-        if(a<1 || a>50){
-            printf("Outside the range");
-        }
-        else{
-            prime(a);
-        }
-        return main();
-    }
-    else if(mc==3){
-        scanf("%d",&a);
-        if(a<1 || a>50){
-            printf("Outside the range");
-        }
-        else{
-            odd_even(a);
-        }
-        return main();
-    }
-    else if(mc==4){
-        exit(0);
     }
 }
