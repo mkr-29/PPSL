3b28283b44bc31c2c42a72a76cf8810e726e9f2e
Mayank Kumar <84685141+mkr-29@users.noreply.github.com>
2021-12-08 14:00:37 +0530

    Update 19B17CI171_P_39.c

--- 19B17CI171_P_39.c
+++ 19B17CI171_P_39.c
@@ -1,28 +1,31 @@
 #include<stdio.h>
 void prime(int x){
     int i,p=0;
-    for(i=2; i<=x/2; i++){
-        if(x%i==0){
-            p=1;
+    if(x<0 || x>100){
+        printf("Outside the range");
+    }
+    else if(x==0 || x==1){
+        printf("Not prime");
+    }
+    else{
+        for(i=2; i<=x/2; i++){
+            if(x%i!=0){
+                continue;
+            }
+            else{
+                p=1;
+            }
+        }
+        if(p==1){
+            printf("Not prime");
         }
         else{
-            p=0;
+            printf("Prime");
         }
-    }//edit the code as you may like
-    if(p==1){
-        printf("Not prime");
-    }//but dont copy just to submit your assignments
-    else{
-        printf("Prime");
     }
 }
 int main(){
     int a;
     scanf("%d",&a);
-    if(a<=0 || a>=100){
-        printf("Outside the range");
-    }
-    else{
-        prime(a);
-    }
+    prime(a);
 }
